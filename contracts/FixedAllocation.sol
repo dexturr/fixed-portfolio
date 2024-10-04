e9aa931ea442344072ac7e9735a663822a668e84
Dexter <dexter@vega.xyz>
2024-10-04 07:00:38 +0700

    chore: create a function for depositing to the profile

--- contracts/FixedAllocation.sol
+++ contracts/FixedAllocation.sol
@@ -1,24 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 // struct Constituent {
 //     address tokenAddress;
 //     uint proportion;
 // }
 
 contract FixedAllocation {
+    // The token that is to be used as the base of this fixed allocation portfolio
+    address public _base_token;
+    // The total amount of the base token that has been deposited into this contract
     uint256 public total_depoisted;
     // The proportions that each index consitutent represents
     mapping(address => uint) public proportions;
+    // The deposits each address has made to this account
+    mapping(address => uint256) public deposits;
 
     // TODO: starting with 2 tokens in an equal split, needs to be generalised later.
     // Step 1, abritary percentages
     // Step 2, arbitary amount of tokens
-    constructor(address token1, address token2) {
+    constructor(address baseToken, address token1, address token2) {
+        // TODO validate that the addresses provided are all ERC20s
+        _base_token = baseToken;
         total_depoisted = 0;
         proportions[token1] = 50;
         proportions[token2] = 50;
         uint totalProportions = proportions[token1] + proportions[token2];
         require(totalProportions == 100, "More than 100% represented");
+    }
+
+    /// @dev This is IStake.staking_token
+    /// @return the address of the token that is able to be staked
+    function base_token() external view returns (address) {
+        return _base_token;
+    }
+
+    function deposit(uint256 amount) public {
+        require(
+            IERC20(_base_token).transferFrom(msg.sender, address(this), amount)
+        );
+        deposits[msg.sender] += amount;
+        total_depoisted += amount;
     }
 }
--- test/FixedAllocation.ts
+++ test/FixedAllocation.ts
@@ -17,25 +17,36 @@ describe("FixedAllocation", function () {
         const [owner, otherAccount] = await hre.ethers.getSigners();
 
         const Token = await hre.ethers.getContractFactory("Token");
+        const wEth = await Token.deploy('Weth', "WETH", TOTAL_SUPPLY);
         const tokenA = await Token.deploy('TokenA', "TKA", TOTAL_SUPPLY);
         const tokenB = await Token.deploy('TokenB', "TKB", TOTAL_SUPPLY);
-        const [addressA, addressB] = await Promise.all([
+        const [addressA, addressB, wethAddress] = await Promise.all([
             tokenA.getAddress(),
-            tokenB.getAddress()
+            tokenB.getAddress(),
+            wEth.getAddress()
         ])
         const FixedAllocation = await hre.ethers.getContractFactory("FixedAllocation");
-        const fixedAllocation = await FixedAllocation.deploy(addressA, addressB)
-        return { tokenA, tokenB, owner, otherAccount, fixedAllocation, addressA, addressB };
+        const fixedAllocation = await FixedAllocation.deploy(wethAddress, addressA, addressB)
+        const fixedAllocationAddress = await fixedAllocation.getAddress();
+        return { tokenA, tokenB, owner, otherAccount, fixedAllocation, addressA, addressB, wethAddress, wEth, fixedAllocationAddress };
     }
 
     describe('FixedAllocation', () => {
         it('Should set constructor properties correctly', async () => {
-            const { tokenA, tokenB, fixedAllocation, addressA, addressB } = await loadFixture(deployBasicFixedAllocation);
+            const { tokenA, tokenB, fixedAllocation, addressA, addressB, wethAddress } = await loadFixture(deployBasicFixedAllocation);
             expect(await tokenA.totalSupply()).to.equal(TOTAL_SUPPLY);
             expect(await tokenB.totalSupply()).to.equal(TOTAL_SUPPLY);
             expect(await fixedAllocation.proportions(addressA)).to.equal(50n)
             expect(await fixedAllocation.proportions(addressB)).to.equal(50n)
             expect(await fixedAllocation.total_depoisted()).to.equal(0n)
+            expect(await fixedAllocation.base_token()).to.equal(wethAddress)
+        })
+        it('increments the total deposited amount and the deposits map when a deposit is made', async () => {
+            const { fixedAllocation, fixedAllocationAddress, owner, wEth } = await loadFixture(deployBasicFixedAllocation);
+            await wEth.approve(fixedAllocationAddress, TOTAL_SUPPLY)
+            await fixedAllocation.deposit(1)
+            expect(await fixedAllocation.total_depoisted()).to.equal(1)
+            expect(await fixedAllocation.deposits(owner)).to.equal(1)
         })
     })
 });
